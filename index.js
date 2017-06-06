b126360d0ca94d7b8a28ae3ea596ea7c547a31a1
Phoenix888Rising <phoenix888rising@gmail.com>
2017-06-05 19:47:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+//;(function(){
+//  let els = document.getElementsByTagName('img'); // locator
+//  console.log(els);
+  // for, while
+//  Array.from(els).forEach(function(el,k){
+//    el.addEventListener('click', function(e){
+//      e.target.parentNode.removeChild(e.target);
+//    });
+//  });
+//})();
+
 ;(function(){
-  let els = document.getElementsByTagName('img');
-  console.log(els);
-  Array.from(els).forEach(function(el,k){
+  let els = document.querySelectorAll('img');
+  els.forEach(function(el,k){
     el.addEventListener('click', function(e){
       e.target.parentNode.removeChild(e.target);
     });
