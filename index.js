ad7b1eaaf52dd8edd287e7b828509f20e5547280
Phoenix888Rising <phoenix888rising@gmail.com>
2017-06-05 19:22:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 ;(function(){
   let elem = document.getElementsByTagName('img');
   console.log(elem);
+  elem.addEventListener('click', function(e){
+    e.parentNode.removeChild(e);
+  });
 })();
